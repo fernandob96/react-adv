a7edf4b5b0afbe875df8f7e71efd38f0ab13215a
Fernando Benavides <fernandousb17@gmail.com>
2023-09-11 16:26:52 -0500

    Control props alternative was implemented

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { IOnChangeArgs, IProduct } from "../interfaces/interfaces";
 
 interface IUseProductArgs {
@@ -11,15 +11,8 @@ export const useProduct = ({onChange, product, value=0}: IUseProductArgs) => {
 
     const [counter, setCounter] = useState(value);
 
-    const ref = useRef(!!onChange);
 
     const increaseBy = ( value: number ) => {
-
-        // Si el onChange existe, maneje su propio cambio de estado
-        if(ref.current && onChange){
-            return onChange({product, count: value});
-        }
-
 
         const newValue = Math.max(counter+value, 0)
         setCounter(newValue);
--- src/02-component-patterns/hooks/useShoppingPageCart.ts
+++ src/02-component-patterns/hooks/useShoppingPageCart.ts
@@ -9,35 +9,16 @@ export const useShoppingPageCart = () => {
 
     const onProductCountChange = ({product, count}: IOnChangeArgs)=> {
         setShoppingCart(prevShoppingCart => {
-    
-          // Find the product or add
-          const getProductInCart: IProductInCard = prevShoppingCart[product.id] || {...product, count: 0};
-    
-          // Si la suma del contador es mayor a 0
-          if(Math.max(getProductInCart.count + count, 0) > 0) {
-            getProductInCart.count += count;
-            return {
-              ...prevShoppingCart,
-              [product.id]: getProductInCart,
-            }
-          } else {
-            // si es menor a 0 (se elimina del listado)
-            delete prevShoppingCart[product.id];
-            return {
-              ...prevShoppingCart
-            }
+        
+          if (count===0){
+            const { [product.id]: toDelete, ...rest } = prevShoppingCart;
+            return rest;
           }
     
-    
-          // if (count===0){
-          //   const { [product.id]: toDelete, ...rest } = prev;
-          //   return rest;
-          // }
-    
-          // return {
-          //   ...prev,
-          //   [product.id] : { ...product, count }
-          // }
+          return {
+            ...prevShoppingCart,
+            [product.id] : { ...product, count }
+          }
         })
       }
 
